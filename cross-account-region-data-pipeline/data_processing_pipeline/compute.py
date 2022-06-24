9ed57837e3cde6bc81f3270ca15fb2b65c649a87
Noah Paige <noahpaig@amazon.com>
2022-06-24 12:17:09 -0400

    Add Cross Account Cross Region Data Pipeline Example

---
+++ cross-account-region-data-pipeline/data_processing_pipeline/compute.py
@@ -0,0 +1,62 @@
+from typing import Any
+from aws_ddk_core.base import BaseStack
+from aws_ddk_core.stages import S3EventStage, SqsToLambdaStage
+from aws_ddk_core.pipelines.pipeline import DataPipeline 
+from constructs import Construct
+from aws_cdk.aws_events import CfnEventBusPolicy
+from aws_cdk.aws_lambda import Code
+import os
+from pathlib import Path
+from typing import Any, Dict
+
+
+class DataProcessingPipeline(BaseStack):
+
+    def __init__(self, scope: Construct, id: str, environment_id: str, params: Dict, mode: str, storage_params: Dict, **kwargs: Any) -> None:
+        super().__init__(scope, id, environment_id, **kwargs)
+        self._params = params
+        self._environment_id = environment_id
+        self._mode = mode
+        self._s3_bucket_name = params.get('s3BucketName')
+        self._storage_account = storage_params.get("account")
+        self._storage_region = storage_params.get("region")
+
+
+        data_lake_s3_event_capture_stage = S3EventStage(
+            self,
+            id=f"s3-event-capture",
+            environment_id=self._environment_id,  
+            event_names=[
+                "Object Created",
+                "Object Deleted"
+            ],
+            bucket_name = self._s3_bucket_name   
+        )
+
+        if self._mode == "cross_account":
+            cfn_event_bus_policy = CfnEventBusPolicy(
+                self, 
+                "StorageAcctEventBusPolicy",
+                statement_id="AllowStorageAccountPutEvents",
+                action="events:PutEvents",
+                principal=self._storage_account
+            )
+
+        sqs_lambda = SqsToLambdaStage(
+            self,
+            "sqs-lambda-stage",
+            environment_id = self._environment_id,
+            code=Code.from_asset(os.path.join(f"{Path(__file__).parents[0]}", "lambdas/processing_lambda")),
+            handler="handler.lambda_handler"
+        )
+
+        self._data_lake_pipeline: DataPipeline = (
+            DataPipeline(
+                self, 
+                id="pipeline_id", 
+                name=f"pipeline_id-pipeline", 
+                description=f"data processing pipeline", 
+            )
+            .add_stage(data_lake_s3_event_capture_stage)
+            .add_stage(sqs_lambda)
+        )
---
+++ cross-account-region-data-pipeline/data_processing_pipeline/storage.py
@@ -0,0 +1,142 @@
+from aws_ddk_core.base import BaseStack
+from aws_ddk_core.resources import S3Factory, KMSFactory
+from aws_ddk_core.stages import S3EventStage 
+from constructs import Construct
+import aws_cdk as cdk
+from aws_cdk.aws_s3 import Bucket, BucketEncryption, BucketAccessControl
+from aws_cdk.aws_events_targets import EventBus
+import aws_cdk.aws_events as events
+from typing import Any, Dict
+from aws_cdk.aws_kms import Key
+from aws_cdk.aws_iam import Effect, AccountPrincipal, PolicyStatement, Role, ServicePrincipal, ManagedPolicy
+from aws_cdk.aws_ssm import StringParameter
+from aws_cdk.aws_events import Rule
+
+class DataStorage(BaseStack):
+
+    def __init__(self, scope: Construct, id: str, environment_id: str, params: Dict, mode: str, compute_params: Dict, **kwargs: Any) -> None:
+        super().__init__(scope, id, environment_id, **kwargs)
+        self._params = params
+        self._environment_id = environment_id
+        self._S3_NAME = params.get('s3BucketName')
+        self._mode = mode
+        self._compute_account = compute_params.get("account")
+        self._compute_region = compute_params.get("region")
+
+        self._raw_bucket = self._create_bucket(name = self._S3_NAME)
+        
+        if self._mode != "same_account_region":
+            self._create_eb_rule()
+
+
+    def _create_bucket(self, name: str) -> Bucket:
+        bucket_key: Key = KMSFactory.key(
+            self,
+            id=f"{name}-bucket-key",
+            environment_id=self._environment_id,
+            description=f"{name.title()} Bucket Key",
+            alias=f"{name}-bucket-key",
+            enable_key_rotation=True,
+            pending_window=cdk.Duration.days(30),
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+        )
+        bucket_key.add_to_resource_policy(
+            statement=PolicyStatement(
+                effect=Effect.ALLOW,
+                principals=[AccountPrincipal(self._compute_account)],
+                actions=[
+                    "kms:CreateGrant",
+                    "kms:Decrypt",
+                    "kms:DescribeKey",
+                    "kms:Encrypt",
+                    "kms:GenerateDataKey*",
+                    "kms:ReEncrypt*"
+                ],
+                resources=["*"],
+            )
+        )
+
+        bucket: Bucket = S3Factory.bucket(
+            self,
+            id=f"{name}-bucket",
+            environment_id = self._environment_id,
+            bucket_name=name,
+            encryption=BucketEncryption.KMS,
+            encryption_key=bucket_key,
+            access_control=BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+            removal_policy=cdk.RemovalPolicy.RETAIN,
+            event_bridge_enabled= True 
+        )
+        bucket.add_to_resource_policy(
+            permission=PolicyStatement(
+                effect=Effect.ALLOW,
+                principals=[AccountPrincipal(self._compute_account)],
+                actions=[
+                    "s3:Get*"
+                ],
+                resources=[f"{bucket.bucket_arn}/*"],
+            )
+        )
+
+        StringParameter(
+            self,
+            f"{name}-bucket-key-arn-ssm",
+            parameter_name=f"/DDK/KMS/{name.title()}BucketKeyArn",
+            string_value=bucket_key.key_arn,
+        )
+
+        StringParameter(
+            self,
+            f"{name}-bucket-arn-ssm",
+            parameter_name=f"/DDK/S3/{name.title()}BucketArn",
+            string_value=bucket.bucket_arn,
+        )
+
+        return bucket
+
+    def _create_eb_rule(self):
+        data_lake_s3_event_capture_stage = S3EventStage(
+            self,
+            id=f"s3-event-capture",
+            environment_id=self._environment_id,  
+            event_names=[
+                "Object Created",
+                "Object Deleted"
+            ],
+            bucket_name = self._raw_bucket.bucket_name   
+        )
+
+        eb_rule = Rule(
+                self,
+                "s3-event-capture-rule",
+                event_pattern=data_lake_s3_event_capture_stage.event_pattern
+        )
+
+        eb_iam_policy = ManagedPolicy(
+            self,
+            "s3-event-capture-policy",
+            statements=[
+                PolicyStatement(
+                    effect=Effect.ALLOW,
+                    actions=["events:PutEvents"],
+                    resources=[
+                        f"arn:aws:events:{self._compute_region}:{self._compute_account}:event-bus/default",
+                        f"arn:aws:events:{self._compute_region}:{self._compute_account}:rule/default/*"
+                    ]
+                )
+            ]
+        )
+        eb_role: Role = Role(
+            self,
+            "s3-event-capture-role",
+            assumed_by=ServicePrincipal("events.amazonaws.com"),
+            role_name= "s3-event-capture-role",
+            managed_policies=[eb_iam_policy]
+        )
+                
+        eb_rule.add_target(
+            EventBus(
+                event_bus = events.EventBus.from_event_bus_arn(self, "External", f"arn:aws:events:{self._compute_region}:{self._compute_account}:event-bus/default"),
+                role = eb_role
+            )
+        )
