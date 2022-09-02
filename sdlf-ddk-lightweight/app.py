61aece200fe12080d746ce1d9ddc780126d3683c
Noah Paige <noahpaig@amazon.com>
2022-09-02 12:11:17 -0400

    DDK SDLF Minor Edits

--- sdlf-ddk-lightweight/app.py
+++ sdlf-ddk-lightweight/app.py
@@ -14,10 +14,39 @@
 
 
 from typing import Any, Dict
+import wrapt
 import aws_cdk as cdk
 from aws_ddk_core.cicd import CICDPipelineStack
 from data_lake.pipelines import SDLFPipelineStack
 from aws_ddk_core.config import Config
+from aws_cdk.pipelines import CodePipeline
+
+
+@wrapt.patch_function_wrapper(CICDPipelineStack, 'build')
+def build(wrapped, self, args, kwargs) -> "CICDPipelineStack":
+    """
+    This is a temporary workaround to expose the publish_assets_in_parallel parameter
+    from CDK into DDK so we can reduce the number of file asset jobs created.
+
+    Returns
+    -------
+    pipeline : CICDPipelineStack
+        CICDPipelineStack
+    """
+
+    publish_assets_in_parallel = kwargs['publish_assets_in_parallel'] if 'publish_assets_in_parallel' in kwargs else True
+    # Create self-mutating CDK Pipeline
+    self._pipeline = CodePipeline(
+        self,
+        id="code-pipeline",
+        cross_account_keys=True,
+        synth=self._synth_action,
+        cli_version=self._config.get_cdk_version(),
+        publish_assets_in_parallel=publish_assets_in_parallel
+    )
+    return self
+
+
 
 class DataLakeFramework(cdk.Stage):
     def __init__(
@@ -41,15 +70,15 @@ config = Config()
 cicd_repository_name = config.get_env_config("cicd").get("repository", "sdlf-ddk-example")
 
 pipeline_name = "sdlf-ddk-pipeline"
-pipeline = (
-    CICDPipelineStack(satellite_app, id=pipeline_name, environment_id="cicd",  pipeline_name=pipeline_name)
-    .add_source_action(repository_name=cicd_repository_name)
-    .add_synth_action()
-    .build()
-    .add_checks()
-    .add_stage("dev", DataLakeFramework(satellite_app, environment_id="dev", pipeline_params=config.get_env_config("dev"), env=config.get_env("dev")))
-    .synth()
-    .add_notifications()
-)   
+pipeline = CICDPipelineStack(satellite_app, id=pipeline_name, environment_id="cicd",  pipeline_name=pipeline_name)
+
+pipeline.add_source_action(repository_name=cicd_repository_name)
+pipeline.add_synth_action()
+pipeline.build(publish_assets_in_parallel=False)
+pipeline.add_checks()
+pipeline.add_stage("dev", DataLakeFramework(satellite_app, environment_id="dev", pipeline_params=config.get_env_config("dev"), env=config.get_env("dev")))
+pipeline.synth()
+pipeline.add_notifications()
+
 
 satellite_app.synth()
