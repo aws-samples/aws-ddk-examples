7ce5d36b8c4db3c387cb276c5ed78b694cecbdd6
kukushking <3997468+kukushking@users.noreply.github.com>
2022-09-05 16:21:49 +0100

    Break the example

--- jam-pipeline/ddk_app/ddk_app_stack.py
+++ jam-pipeline/ddk_app/ddk_app_stack.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from aws_cdk.aws_lambda import Code, LayerVersion
+from aws_cdk.aws_s3 import Bucket
 from aws_ddk_core.base import BaseStack
 from aws_ddk_core.pipelines import DataPipeline
 from aws_ddk_core.resources import S3Factory
@@ -15,10 +16,9 @@ class DdkApplicationStack(BaseStack):
         super().__init__(scope, id, environment_id, **kwargs)
 
         # The code that defines your stack goes here. For example:
-        ddk_bucket = S3Factory.bucket(
+        ddk_bucket = Bucket(
             self,
             "ddk-bucket",
-            environment_id,
             event_bridge_enabled=True,
         )
 
@@ -35,13 +35,6 @@ class DdkApplicationStack(BaseStack):
             environment_id=environment_id,
             code=Code.from_asset("./lambda"),
             handler="index.lambda_handler",
-            layers=[
-                LayerVersion.from_layer_version_arn(
-                    self,
-                    "ddk-lambda-layer-wrangler",
-                    f"arn:aws:lambda:{self.region}:336392948345:layer:AWSDataWrangler-Python39:2",
-                )
-            ]
         )
         ddk_bucket.grant_read_write(sqs_lambda_stage.function)
 
